dd27418b7717d83a628984e4646c11214f635319
DevHiv3 <dev.hiv3e@gmail.com>
2025-05-12 03:44:00 +0530

    CRUD LAWYERS, EVENTS & LINKS (MINOR CHANGES)

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,6 +1,6 @@
 import GalleryPage from "./gallery.jsx";
 
-const base_url = "http://localhost:8080";
+const base_url = "https://bba-backend.onrender.com";
 
 export default async function GallerySite(){
   const res = await fetch(`${base_url}/links`, { cache: "no-store" });
