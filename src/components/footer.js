c61fe76a1f675694eaf52112a447fb3caf658a2c
DevHiv3 <dev.hiv3e@gmail.com>
2025-06-19 04:40:33 +0530

    Force redeploy 2

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,9 +18,7 @@ function Footer(){
                   <span className="text-left banner-text text-gray-300 text-sm font-normal hover:shadow-xl hover:text-blue-400 transition-transform duration-200 ease-in-out mx-4 banner-text">company</span>
                 </Link>
 
-                <Link className="self-start" href="/">
-                  <span className="text-left banner-text text-gray-300 text-sm  font-normal hover:shadow-xl over:text-gray-400 transition-transform duration-200 ease-in-out mx-4 banner-text">About Us</span>
-                </Link>
+      
 
             </div>
 
