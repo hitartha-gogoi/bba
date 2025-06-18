6ec5d4f81295d4a943b1286a707c2b857a320dfe
DevHiv3 <dev.hiv3e@gmail.com>
2025-06-19 03:35:14 +0530

    FINAL: UPDATED SERVER URL: api.bahadurgarh.com: 2

--- src/components/footer.js
+++ src/components/footer.js
@@ -26,10 +26,6 @@ function Footer(){
                   <span className="text-left banner-text text-gray-300 text-sm  font-normal hover:shadow-xl hover:text-blue-400 transition-transform duration-200 ease-in-out mx-4 banner-text">Our Team</span>
                 </Link>
 
-                <Link className="self-start" href="/">
-                  <span className="text-left banner-text text-gray-300 text-sm  font-normal hover:shadow-xl hover:text-blue-400 transition-transform duration-200 ease-in-out mx-4 banner-text">Blog</span>
-                </Link>
-
             </div>
 
            {/* ONE COLUMN */}
